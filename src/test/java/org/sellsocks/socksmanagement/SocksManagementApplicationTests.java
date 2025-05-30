2d10df999974b45fc320c30cd87d2cd985752808
dmitridorje <dmitridorje@gmail.com>
2025-05-30 21:23:10 +0300

    general test disabled

--- src/test/java/org/sellsocks/socksmanagement/SocksManagementApplicationTests.java
+++ src/test/java/org/sellsocks/socksmanagement/SocksManagementApplicationTests.java
@@ -1,8 +1,10 @@
 package org.sellsocks.socksmanagement;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Not needed for now")
 @SpringBootTest
 class SocksManagementApplicationTests {
 
